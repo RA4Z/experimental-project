682fef4e7ef45b10fb7ef655876e7c0f0878496c
RA4Z <rraz639@gmail.com>
2023-06-11 10:52:04 -0300

    Criação das rotas de login e cadastro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import Routes from './src/Routes';
+
+import {TEMAS} from './src/styles/temas';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Routes />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -11,7 +11,8 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name='Login' component={Login as React.FC} />
+                <Tab.Screen name='Login' component={Login as React.FC} options={{headerShown:false}} />
+                <Tab.Screen name='Cadastro' component={Cadastro as React.FC} options={{headerShown:false}} />
             </Tab.Navigator>
         </NavigationContainer>
     )
