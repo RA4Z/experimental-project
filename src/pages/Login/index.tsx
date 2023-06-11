2032f30ee35d13bd178b69ce6bcf88ffbe3b93c8
RA4Z <rraz639@gmail.com>
2023-06-11 11:12:25 -0300

    Colocando animação lottie no projeto

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,20 @@
-import { VStack, Text } from "native-base";
 import React from "react";
+import { VStack, Text } from "native-base";
+import LottieView from 'lottie-react-native'
 
 export default function Login() {
+
     return (
-        <VStack>
+        <VStack flex={1} alignItems="center" p={5}>
+            <LottieView 
+                source={require('../../assets/dog-academy.json')}
+                autoPlay={true}
+                style={{
+                    width: 200,
+                    height: 200,
+                }}
+                loop={true}
+            />
             <Text>Esta é a tela de Login</Text>
         </VStack>
     )
