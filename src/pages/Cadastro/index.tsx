fd384daad722a8ad9ad083a218c604f5ba8192a7
RA4Z <rraz639@gmail.com>
2023-06-13 19:07:38 -0300

    Desenvolmento da tela de cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -3,14 +3,27 @@ import React, { useState } from "react";
 import { Titulo } from "../../components/Titulo";
 import { InputTexto } from "../../components/InputTexto";
 import Logo from "../../components/Logo";
+import { Botao } from "../../components/Botao";
 
 export default function Cadastro() {
     const [nome, setNome] = useState('')
+    const [secao, setSecao] = useState(0)
+
+    function avancarSecao() {
+        setSecao(secao + 1)
+    }
+
     return (
         <VStack flex={1} alignItems="center" justifyContent='flex-start' p={5}>
-            <Logo height={200} width={200} />
-            <Titulo>Por favor, nos diga como podemos lhe chamar...</Titulo>
-            <InputTexto placeholder='Nome de usuário' value={nome} onChangeText={setNome}  />
+            {secao == 0 && <>
+                <Logo height={200} width={200} />
+                <Titulo>Por favor, nos diga como podemos lhe chamar...</Titulo>
+                <InputTexto placeholder='Nome de usuário' value={nome} onChangeText={setNome}  /> 
+            </>}
+            {secao == 1 && <>
+                <Titulo>Olá {nome}, tudo certo?</Titulo>
+            </>}
+            <Botao onPress={avancarSecao}>Avançar</Botao>
         </VStack>
     )
 }
