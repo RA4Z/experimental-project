7d2dc664247503f68fbcc624f4d38b4ab4452f05
RA4Z <rraz639@gmail.com>
2023-06-13 21:26:48 -0300

    Continuidade na tela de cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -5,23 +5,42 @@ import { InputTexto } from "../../components/InputTexto";
 import Logo from "../../components/Logo";
 import { Botao } from "../../components/Botao";
 
+import { secoes } from '../../utils/CadastroInputs';
+
 export default function Cadastro() {
-    const [nome, setNome] = useState('')
+    const [username, setUsername] = useState('')
+    const [dados, setDados] = useState({} as any)
     const [secao, setSecao] = useState(0)
 
     function avancarSecao() {
         setSecao(secao + 1)
     }
 
+    function atualizarDados(id: string, valor: string) {
+        setDados({...dados, [id]: valor})
+      }
+
     return (
         <VStack flex={1} alignItems="center" justifyContent='flex-start' p={5}>
             {secao == 0 && <>
                 <Logo height={200} width={200} />
                 <Titulo>Por favor, nos diga como podemos lhe chamar...</Titulo>
-                <InputTexto placeholder='Nome de usuário' value={nome} onChangeText={setNome}  /> 
+                <InputTexto placeholder='Nome de usuário' value={username} onChangeText={setUsername}  /> 
             </>}
             {secao == 1 && <>
-                <Titulo>Olá {nome}, tudo certo?</Titulo>
+                <Titulo>Olá {username}, pedimos que insira alguns dados sobre você</Titulo>
+                {secoes[0]?.entradaTexto.map(entrada => {
+                    return (
+                        <InputTexto 
+                            key={entrada.id} 
+                            placeholder={entrada.placeholder}
+                            label={entrada.label}
+                            value={dados[entrada.name]}
+                            secureTextEntry={entrada.secureTextEntry}
+                            onChangeText={(text) => atualizarDados(entrada.name, text)}
+                        />
+                    )
+                })}
             </>}
             <Botao onPress={avancarSecao}>Avançar</Botao>
         </VStack>
--- src/utils/CadastroInputs.ts
+++ src/utils/CadastroInputs.ts
@@ -1,1 +1,41 @@
-// Criar Inputs do Cadastro aqui para otimização
+const secoes = [
+    {
+      id: 1,
+      titulo: 'Olá nome, pedimos que insira alguns dados básicos',
+      entradaTexto: [
+        {
+          id: 1,
+          label: 'Nome',
+          placeholder: 'Digite seu nome completo',
+          name: 'nome'
+        },
+        {
+          id: 2,
+          label: 'E-mail',
+          placeholder: 'Digite seu E-mail',
+          name: 'email'
+        },
+        {
+          id: 3,
+          label: 'Telefone',
+          placeholder: '(00) 00000-0000',
+          name: 'senha'
+        },
+        {
+          id: 4,
+          label: 'Senha',
+          placeholder: 'Insira sua senha',
+          secureTextEntry: true,
+          name: 'senha'
+        },
+        {
+          id: 5,
+          label: 'Confirme a senha',
+          placeholder: 'Insira sua senha',
+          secureTextEntry: true,
+          name: 'confirmaSenha'
+        }
+      ]
+    }
+  ]
+  export { secoes }
