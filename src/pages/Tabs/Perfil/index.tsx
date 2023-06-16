5737be9df799da0ea91f9b11c386c74cf283a34c
RA4Z <rraz639@gmail.com>
2023-06-16 19:13:52 -0300

    Estilizando a tela de perfil com ícones

--- src/pages/Tabs/Perfil/index.tsx
+++ src/pages/Tabs/Perfil/index.tsx
@@ -1,12 +1,78 @@
-import { VStack, Avatar } from "native-base";
+import { VStack, Avatar, Box, Text, ScrollView, Divider, Image } from "native-base";
 import { Titulo } from "../../../components/Titulo";
+import { TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+
+
+import ExerciciosMascIMG from './assets/ginasticaMasc.png'
+import ExerciciosFemiIMG from './assets/ginasticaFemi.png'
+
+import SetaDireitaIMG from './assets/seta-direita.png'
+import AlunosIMG from './assets/alunos.png'
+import CalendarioIMG from './assets/calendario.png'
+import HistoricoIMG from './assets/historico.png'
 
 export default function Perfil() {
+    const [role, setRole] = useState('Professor')
+    const [genero, setGenero] = useState('Masculino')
+    const [cargo, setCargo] = useState(role)
+
+    useEffect(() => {
+        if(genero != 'Masculino') {
+            if(role == 'Professor') setCargo('Professora')
+            if(role == 'Aluno') setCargo('Aluna')
+        }
+    },[genero])
     return (
-        <VStack p={5} justifyContent='center' alignItems='center'>
+        <VStack flex={1} justifyContent='flex-start' alignItems='center'>
             <Titulo fontSize='lg'>Robert Aron Zimmermann</Titulo>
             <Avatar size={150} source={{ uri: 'https://github.com/RA4Z.png' }} mt={5} />
-                
+
+            <Box flexDirection='row' backgroundColor='blue.800' mt={5} w='100%' h={50} justifyContent='space-around' alignItems='center' >
+                <Text color="white" fontWeight="bold" fontSize='md'>{cargo}</Text>
+            </Box>
+            <ScrollView backgroundColor='#05668d' height='100%' w='100%'>
+                <Box flex={1} p={5}>
+
+                    <TouchableOpacity>
+                        <VStack alignItems='center' justifyContent='space-around' flexDirection='row'>
+                            <Image source={CalendarioIMG} mt={5} w={50} h={50} alt="Exercícios" />
+                            <Titulo color='white'>Treinos</Titulo>
+                            <Image source={SetaDireitaIMG} mt={5} w={50} h={50} alt="Seta Direita" />
+                        </VStack>
+                    </TouchableOpacity>
+                    <Divider mt={5} />
+                    
+                    <TouchableOpacity>
+                        <VStack alignItems='center' justifyContent='space-around' flexDirection='row'>
+                            <Image source={HistoricoIMG} mt={5} w={50} h={50} alt="Exercícios" />
+                            <Titulo color='white'>IMC</Titulo>
+                            <Image source={SetaDireitaIMG} mt={5} w={50} h={50} alt="Seta Direita" />
+                        </VStack>
+                    </TouchableOpacity>
+                    <Divider mt={5} />
+
+                    <TouchableOpacity>
+                        <VStack alignItems='center' justifyContent='space-around' flexDirection='row'>
+                            {genero == 'Masculino' ? <Image source={ExerciciosMascIMG} mt={5} w={50} h={50} alt="Exercícios" /> :
+                            <Image source={ExerciciosFemiIMG} mt={5} w={50} h={50} alt="Exercícios" />}
+                            <Titulo color='white'>Evolução</Titulo>
+                            <Image source={SetaDireitaIMG} mt={5} w={50} h={50} alt="Seta Direita" />
+                        </VStack>
+                    </TouchableOpacity>
+                    <Divider mt={5} />
+
+                    {role == 'Professor' && 
+                    <TouchableOpacity>
+                        <VStack alignItems='center' justifyContent='space-around' flexDirection='row'>
+                            <Image source={AlunosIMG} mt={5} w={50} h={50} alt="Exercícios" />
+                            <Titulo color='white'>Alunos</Titulo>
+                            <Image source={SetaDireitaIMG} mt={5} w={50} h={50} alt="Seta Direita" />
+                        </VStack>
+                    </TouchableOpacity>
+                    }
+                </Box>
+            </ScrollView>
         </VStack>
     )
 }
